2eef2f8f6eafe972fe7f7992752bb9d2cf0d2af0
jensbaetens-odisee <83013151+jensbaetens-odisee@users.noreply.github.com>
2021-05-04 17:46:40 +0200

    Overlopen oefeningen dag 1 en 2

---
+++ Lesdag2/Exercises/NumbersOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class NumbersOperations
+    {
+
+        int[] numbers;
+
+        public NumbersOperations(int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public IEnumerable<int> GetAllNumbersLargerOrEqualTo50()
+        {
+            return numbers.Where(x => x >= 50);
+        }
+
+        public int GetLastNumberWithSquareLessThan2000()
+        {
+           // Math.Sqrt(numbers.Select(x => x * x).Last(x => x < 2000));
+            return numbers.Last(x => x * x < 2000);
+        }
+
+    }
+}
---
+++ Lesdag2/ExercisesTests/NumberOperationsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Exercises;
+using System.Linq;
+
+namespace ExercisesTests
+{
+    [TestFixture]
+    class NumberOperationsTests
+    {
+        [Test]
+        public void GetAllNumbersLargerOrEqualTo50_HappyFlow_ReturnsCorrectNumbers()
+        {
+            //Arrange
+            int[] array = { 1, 200, 3 };
+            NumbersOperations operations = new NumbersOperations(array);
+
+            //Act
+            IEnumerable<int> result = operations.GetAllNumbersLargerOrEqualTo50();
+
+            //Assert
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.Last, Is.EqualTo(50));
+            Assert.That(result.First, Is.EqualTo(200));
+            Assert.That(result.ElementAt(0), Is.EqualTo(200));
+            Assert.That(result.ElementAt(1), Is.EqualTo(300));
+            Assert.That(result.ElementAt(2), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void GetLastNumberWithSquareLessThan2000()
+        {
+            //Arrange
+            int[] array = { 1, 200, 3, 300, 4, 500, 50 };
+            NumbersOperations operations = new NumbersOperations(array);
+
+            //Act
+            int result = operations.GetLastNumberWithSquareLessThan2000();
+
+            //Assert
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
